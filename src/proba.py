b4a0ae24845c54d1005c7f41078afb0e0a2810f0
fpetric <f5r1c.1m0@gmail.com>
2017-03-29 17:33:48 +0200

    Fixed issues with file reorganization

--- src/proba.py
+++ src/proba.py
@@ -1,6 +1,6 @@
 import xml.etree.ElementTree as ET
-e=ET.parse('Tiger.pomdpx').getroot()
-tree = ET.parse('Tiger.pomdpx')
+e=ET.parse('../examples/Tiger.pomdpx').getroot()
+tree = ET.parse('../examples/Tiger.pomdpx')
 root = tree.getroot()
 for child in root:
     print child.tag, child
