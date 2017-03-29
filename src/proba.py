8a08bb735670d5af44fa3ed0e66e7c91b2bd81f3
fpetric <f5r1c.1m0@gmail.com>
2017-03-29 17:46:12 +0200

    Did something

--- src/proba.py
+++ src/proba.py
@@ -3,5 +3,12 @@ e=ET.parse('../examples/Tiger.pomdpx').getroot()
 tree = ET.parse('../examples/Tiger.pomdpx')
 root = tree.getroot()
 for child in root:
-    print child.tag, child
+    if child.tag == 'Description' or child.tag == 'Discount':
+        print(child.tag, child.text)
+    elif child.tag == "Variable":
+        for state_var in child.findall('StateVar'):
+            print(state_var.tag, state_var.attrib, state_var.text)
+            value_enum = state_var.findall('ValueEnum')
+            print(value_enum[0].text)
+        # print(child.tag, child.list(child.tag))
 
